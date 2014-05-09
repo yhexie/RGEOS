703a842b405501d8e088bddbe708c7e9f021e2a5
yhexie <yhlx125@163.com>
2014-05-09 21:20:23 +0800

    Icons
    Signed-off-by: yhexie <yhlx125@163.com>

--- RGeos.Desktop/MainFrm.Designer.cs
+++ RGeos.Desktop/MainFrm.Designer.cs
@@ -28,15 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.工具ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tspPan = new System.Windows.Forms.ToolStripMenuItem();
             this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.labcoord = new System.Windows.Forms.ToolStripStatusLabel();
             this.labInfo = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.labcoord = new System.Windows.Forms.ToolStripStatusLabel();
             this.labCoordinate = new System.Windows.Forms.ToolStripStatusLabel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
@@ -47,18 +48,18 @@
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.文件ToolStripMenuItem,
-            this.工具ToolStripMenuItem,
-            this.编辑ToolStripMenuItem});
+            this.编辑ToolStripMenuItem,
+            this.工具ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(553, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(690, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // 文件ToolStripMenuItem
             // 
             this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
-            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.文件ToolStripMenuItem.Text = "文件";
             // 
             // 工具ToolStripMenuItem
@@ -66,20 +67,21 @@
             this.工具ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tspPan});
             this.工具ToolStripMenuItem.Name = "工具ToolStripMenuItem";
-            this.工具ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.工具ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.工具ToolStripMenuItem.Text = "工具";
             // 
             // tspPan
             // 
+            this.tspPan.Image = ((System.Drawing.Image)(resources.GetObject("tspPan.Image")));
             this.tspPan.Name = "tspPan";
-            this.tspPan.Size = new System.Drawing.Size(100, 22);
+            this.tspPan.Size = new System.Drawing.Size(152, 22);
             this.tspPan.Text = "漫游";
             this.tspPan.Click += new System.EventHandler(this.tspPan_Click);
             // 
             // 编辑ToolStripMenuItem
             // 
             this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
-            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
             this.编辑ToolStripMenuItem.Text = "编辑";
             // 
             // statusStrip1
@@ -90,24 +92,17 @@
             this.toolStripStatusLabel2,
             this.labcoord,
             this.labCoordinate});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 321);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 423);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(553, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(690, 22);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
-            // 
-            // labcoord
-            // 
-            this.labcoord.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.labcoord.Name = "labcoord";
-            this.labcoord.Size = new System.Drawing.Size(44, 17);
-            this.labcoord.Text = "坐标：";
             // 
             // labInfo
             // 
             this.labInfo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.labInfo.Name = "labInfo";
-            this.labInfo.Size = new System.Drawing.Size(32, 17);
+            this.labInfo.Size = new System.Drawing.Size(29, 17);
             this.labInfo.Text = "就绪";
             // 
             // toolStripStatusLabel2
@@ -115,8 +110,15 @@
             this.toolStripStatusLabel2.BorderStyle = System.Windows.Forms.Border3DStyle.Adjust;
             this.toolStripStatusLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
-            this.toolStripStatusLabel2.Size = new System.Drawing.Size(462, 17);
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(605, 17);
             this.toolStripStatusLabel2.Spring = true;
+            // 
+            // labcoord
+            // 
+            this.labcoord.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.labcoord.Name = "labcoord";
+            this.labcoord.Size = new System.Drawing.Size(41, 17);
+            this.labcoord.Text = "坐标：";
             // 
             // labCoordinate
             // 
@@ -126,16 +128,16 @@
             // panel1
             // 
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(553, 296);
+            this.panel1.Size = new System.Drawing.Size(690, 399);
             this.panel1.TabIndex = 2;
             // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(553, 343);
+            this.ClientSize = new System.Drawing.Size(690, 445);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
